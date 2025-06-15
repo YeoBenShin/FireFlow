24c5431a1bc9d7746b0ac008be72648bef827cd9
YeoBenShin <orang3cats@gmail.com>
2025-06-15 12:30:20 +0800

    fix: updated port number to 5100 because mac uses port 5000 for airplay receiver

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ import transactionRoutes from './routes/transactionRoutes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 
 app.use(cors());
 app.use(express.json());
