77ba6b61fca68b651b4943a3acad45bca8868d7d
YeoBenShin <orang3cats@gmail.com>
2025-07-19 01:40:48 +0800

    changed the vercel structure

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -49,4 +49,4 @@ app.get("/", (_req, res) => {
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
-module.exports = app;
+export default app;
---
+++ backend/start.ts
@@ -0,0 +1,7 @@
+import app from "./api/server";
+
+const PORT = process.env.PORT || 5100;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
