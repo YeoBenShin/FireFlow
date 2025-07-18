b245d8c9434b7b61dc1478ab3cec0099856d222d
YeoBenShin <orang3cats@gmail.com>
2025-07-19 01:02:18 +0800

    adjust module for vercel

--- backend/server.ts
+++ backend/server.ts
@@ -46,6 +46,7 @@ app.get("/", (_req, res) => {
   res.send("API is running...");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = app;
