6475d71ff5a5018871d692dd03b7bd8d043e1bc3
YeoBenShin <orang3cats@gmail.com>
2025-07-18 23:21:41 +0800

    changed name of token

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -16,10 +16,10 @@ export async function middleware(request: NextRequest) {
 
   // Validate session with backend
   try {
-    console.log(localStorage.getItem('jwtToken'));
+    console.log(localStorage.getItem('authToken'));
     const res = await fetch("https://fireflow-m0z1.onrender.com/api", {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
       },
     });
 
