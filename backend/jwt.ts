7b7886c8cc62a55aed3e96b61e52fc33953733d7
YeoBenShin <orang3cats@gmail.com>
2025-07-19 21:47:41 +0800

    added more check to jwt.ts

--- backend/jwt.ts
+++ backend/jwt.ts
@@ -10,7 +10,7 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(' ')[1];
   // console.log(`[DEBUG] Verifying JWT:`, token);
 
-  if (!token) {
+  if (!token || token === undefined) {
     res.status(401).json({ error: 'No token provided' });
     return;
   }
